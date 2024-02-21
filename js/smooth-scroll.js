bafff7fe2f5435f2297bfe384d1990fa376993ae
doankietdev <doankietdev@gmail.com>
2024-02-21 14:58:08 +0700

    edit

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -26,7 +26,7 @@ $('a.smooth-scroll')
       event.preventDefault();
       $('html, body').animate({
         scrollTop: target.offset().top
-      }, 1000, function() {
+      }, 500, function() {
         // Callback after animation
         // Must change focus!
         var $target = $(target);
